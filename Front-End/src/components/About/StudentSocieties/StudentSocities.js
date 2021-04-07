ca29d12a767a5bdbe6647fffb4fea7747ab002ed
shreiyarandive <shreiyarandive1@gmail.com>
2021-04-07 09:42:10 +0530

    Component Folder

---
+++ Front-End/src/components/About/StudentSocieties/StudentSocities.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import 'F:/React/ieee-pune-section-website/Front-End/src/components/About/StudentSocieties/StudentSocities.css';
+export class StudentSocities extends Component {
+    render() {
+        return (
+            <div>
+                <div className="box">
+           
+                </div>
+            </div>
+        )
+    }
+}
+
+export default StudentSocities
